c5966adb64cd0e4b6fc45d48119fbe612bc8ca1f
YuXiang <richardlin0212@gmail.com>
2019-12-04 14:33:43 +0800

    Modify .gitignore & test.ZhuMon Add *.swp in .gitignore Modify print_event.py more modularize

--- test/ZhuMon/print_event.py
+++ test/ZhuMon/print_event.py
@@ -8,7 +8,29 @@ sys.path.append(
 
 import lib.sc2reader as sc2reader
 
-replay = sc2reader.load_replay('./replay_for_test.SC2Replay', load_map=True)
+def parse_replay(replay):
+    """
+    type: 
+        Dict{replay_dict} 
+        v 
+        List[List[sc2reader.events.game.GameEvent], ...] 
+        # of inside List based on # of players
+    """
+    players = list(replay.player.values())
+    result = [[] for p in players] # create List[List[], List[], ...] 
+    for event in replay.game_events:
+        try:
+            player_of_event = players.index(event.player)
+            result[player_of_event].append(event)
+        except:
+            continue
 
-for i in replay.game_events:
-    print(i)
+    return result
+
+if __name__ == "__main__":
+    replay = sc2reader.load_replay('./replay_for_test.SC2Replay', load_map=True)
+    result = parse_replay(replay)
+    for r in result:
+        for e in r:
+            print(e)
+
