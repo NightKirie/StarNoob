618ccc6f04bd7073a458973fc8de2a06546fce10
frank870622 <frank870622@gmail.com>
2020-01-01 22:31:04 +0800

    Fix small bug

--- sub_policy_economic.py
+++ sub_policy_economic.py
@@ -496,7 +496,7 @@ class SubAgent_Economic(Agent):
         total_value_structures_score = obs.observation.score_cumulative.total_value_structures
         total_spent_minerals = obs.observation.score_cumulative.spent_minerals
         total_spent_vespene = obs.observation.score_cumulative.spent_vespene
-        building_num = self.get_my_building_by_pos(obs)
+        building_num = len(self.get_my_building_by_pos(obs))
 
         positive_reward = 0
         ## Positive reward update in this step
